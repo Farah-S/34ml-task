edc9e499c6f91d30f2b49b66ee5bb3efc49862e5
Farah Sherif <farah.faroha132@gmail.com>
2024-07-04 22:28:27 +0300

    lesson factory

---
+++ database/factories/LessonFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Lesson;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Lesson>
+ */
+class LessonFactory extends Factory
+{
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Lesson::class;
+
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->title(),
+            'order' => fake()->numberBetween(1,20),
+        ];
+    }
+}
