9a5e4cab40f12b2009b79f08a3c1aac1255ede42
Farah Sherif <farah.faroha132@gmail.com>
2024-07-05 18:50:48 +0300

    course layout done

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -27,79 +27,67 @@
                             <div class="flex items-center justify-center w-9 h-9 bg-white border-2 border-gray-800 rounded-full">
                                 <span>1</span>
                             </div>
-                            </div>
-                            <div class="min-w-0 flex-auto">
+                        </div>
+                        <div class="min-w-0 flex-auto">
                                 <p class="text-sm font-semibold leading-6 text-gray-900">Lesson Name</p>
                                 <a href="" class="mt-1 truncate text-xs leading-5 blue-link">start lesson</a>
-                            </div>
                         </div>
-                        <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                        <p class="text-sm leading-6 text-gray-900">Business Relations</p>
-                        <div class="mt-1 flex items-center gap-x-1.5">
-                            <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-                            <div class="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
-                            </div>
-                            <p class="text-xs leading-5 text-gray-500">Online</p>
-                        </div>
+                    </div>
+                    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                        <p class="text-sm leading-6 text-gray-900">Completed</p>
+                        
                     </div>
                 </li>
                 <li class="flex justify-between gap-x-6 py-5">
                     <div class="flex min-w-0 gap-x-4">
                         <div class="flex items-center justify-center ">
                             <div class="flex items-center justify-center w-9 h-9 bg-white border-2 border-gray-800 rounded-full">
-                                <span>5</span>
+                                <span>1</span>
                             </div>
                         </div>
                         <div class="min-w-0 flex-auto">
-                            <p class="text-sm font-semibold leading-6 text-gray-900">Lindsay Walton</p>
-                            <p class="mt-1 truncate text-xs leading-5 text-gray-500">lindsay.walton@example.com</p>
+                                <p class="text-sm font-semibold leading-6 text-gray-900">Lesson Name</p>
+                                <a href="" class="mt-1 truncate text-xs leading-5 blue-link">start lesson</a>
                         </div>
                     </div>
                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                    <p class="text-sm leading-6 text-gray-900">Front-end Developer</p>
-                    <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
+                        <p class="text-sm leading-6 text-gray-900">Completed</p>
+                        
                     </div>
-                </li>
-                <li class="flex justify-between gap-x-6 py-5">
+                </li><li class="flex justify-between gap-x-6 py-5">
                     <div class="flex min-w-0 gap-x-4">
                         <div class="flex items-center justify-center ">
                             <div class="flex items-center justify-center w-9 h-9 bg-white border-2 border-gray-800 rounded-full">
-                                <span>5</span>
+                                <span>1</span>
                             </div>
                         </div>
                         <div class="min-w-0 flex-auto">
-                            <p class="text-sm font-semibold leading-6 text-gray-900">Courtney Henry</p>
-                            <p class="mt-1 truncate text-xs leading-5 text-gray-500">courtney.henry@example.com</p>
+                                <p class="text-sm font-semibold leading-6 text-gray-900">Lesson Name</p>
+                                <a href="" class="mt-1 truncate text-xs leading-5 blue-link">start lesson</a>
                         </div>
                     </div>
                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                    <p class="text-sm leading-6 text-gray-900">Designer</p>
-                    <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
+                        <p class="text-sm leading-6 text-gray-900">Completed</p>
+                        
                     </div>
-                </li>
-                <li class="flex justify-between gap-x-6 py-5">
+                </li><li class="flex justify-between gap-x-6 py-5">
                     <div class="flex min-w-0 gap-x-4">
                         <div class="flex items-center justify-center ">
                             <div class="flex items-center justify-center w-9 h-9 bg-white border-2 border-gray-800 rounded-full">
-                                <span>5</span>
+                                <span>1</span>
                             </div>
                         </div>
                         <div class="min-w-0 flex-auto">
-                            <p class="text-sm font-semibold leading-6 text-gray-900">Tom Cook</p>
-                            <p class="mt-1 truncate text-xs leading-5 text-gray-500">tom.cook@example.com</p>
+                                <p class="text-sm font-semibold leading-6 text-gray-900">Lesson Name</p>
+                                <a href="" class="mt-1 truncate text-xs leading-5 blue-link">start lesson</a>
                         </div>
                     </div>
                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                    <p class="text-sm leading-6 text-gray-900">Director of Product</p>
-                    <div class="mt-1 flex items-center gap-x-1.5">
-                        <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-                        <div class="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
-                        </div>
-                        <p class="text-xs leading-5 text-gray-500">Online</p>
-                    </div>
+                        <p class="text-sm leading-6 text-gray-900">Completed</p>
+                        
                     </div>
                 </li>
-                </ul>
+            </ul>
 
         </div>
     </div>
