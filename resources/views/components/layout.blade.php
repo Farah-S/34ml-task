5f92a6760236acc8fd10894b47c94b13617ff272
Farah Sherif <farah.faroha132@gmail.com>
2024-07-05 19:57:47 +0300

    title change

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Home</title>
+        <title>34ml</title>
         
         <script src="https://cdn.tailwindcss.com"></script>
         <!-- Fonts -->
