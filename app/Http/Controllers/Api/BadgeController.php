5c78b9c8d8d905bb0497b9aff91d2a58a3f09d9d
Farah Sherif <farah.faroha132@gmail.com>
2024-07-06 08:58:56 +0300

    hot fix

--- app/Http/Controllers/Api/BadgeController.php
+++ app/Http/Controllers/Api/BadgeController.php
@@ -14,10 +14,21 @@ class BadgeController extends Controller
         $user = Auth::user();
   
         $currentBadge=$user->badges()->latest('badge_user.created_at')->get();
-        $nextBadge=Badge::where('id', '>', $currentBadge[count($currentBadge) - 1]->id)->orderBy('id')->first();
-        $remaining=$nextBadge->required_ach - $currentBadge[count($currentBadge) - 1]->required_ach;
+        if(count($currentBadge)>0){
+
+            $nextBadge=Badge::where('id', '>', $currentBadge[count($currentBadge) - 1]->id)->orderBy('id')->first();
+            $remaining=$nextBadge->required_ach - $currentBadge[count($currentBadge) - 1]->required_ach;
+            return response()->json(['status' => 'success', 'current_badge' => $currentBadge[count($currentBadge) - 1]->title ,'next_badge'=>$nextBadge->title, 'remaining_to_unlock_next_badge'=>$remaining]);
+        }
+        else{
+            $currentBadge=Badge::orderBy('id', 'asc')->first();
+            $nextBadge=Badge::where('id', '>', $currentBadge->id)->orderBy('id')->first();
+            $remaining=$nextBadge->required_ach - $currentBadge->required_ach;
+
+            return response()->json(['status' => 'success', 'current_badge' => $currentBadge->title ,'next_badge'=>$nextBadge->title, 'remaining_to_unlock_next_badge'=>$remaining]);
+
+        }
         
-        return response()->json(['status' => 'success', 'current_badge' => $currentBadge[count($currentBadge) - 1]->title ,'next_badge'=>$nextBadge->title, 'remaining_to_unlock_next_badge'=>$remaining]);
     
     }
 }
